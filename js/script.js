7837272485ce582d7208931bf1e03e8cd59851a2
yaredghebre <yared2391@gmail.com>
2023-03-22 11:56:59 -0300

    Add: Project Scaffolding

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	            scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1:
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede. Se non vi sentite particolarmente creativi, potete prendere uno spunto dallo screenshot allegato.
