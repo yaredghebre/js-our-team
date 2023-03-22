120a2c4b4b830152431f5942c84ad6d8dbba81e1
yaredghebre <yared2391@gmail.com>
2023-03-22 13:43:29 -0300

    Add: Project complete (bonus 1-2)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
-// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-// Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
-// Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
-// Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
-// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
-// Scott Estrada	Developer	            scott-estrada-developer.jpg
-// Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
 // MILESTONE 0:
 // Creare l’array di oggetti con le informazioni fornite.
 // MILESTONE 1:
@@ -16,3 +8,68 @@
 // Trasformare la stringa foto in una immagine effettiva
 // BONUS 2:
 // Organizzare i singoli membri in card/schede. Se non vi sentite particolarmente creativi, potete prendere uno spunto dallo screenshot allegato.
+
+
+// Creo ARRAY con MEMBRI TEAM e al suo interno gli OGGETTI
+const teamMembers = [
+    {
+        name: "Wayne Barnett",
+        position: "Founder & CEO",
+        picture: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Carroll",
+        position: "Chief Editor",
+        picture: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        position: "Office Manager",
+        picture: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        position: "Social Media Manager",
+        picture: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        position: "Developer",
+        picture: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        position: "Graphic Designer",
+        picture: "barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+// STAMPO IN CONSOLE i dati dei TEAM MEMBERS
+// CREO CICLO FOR
+// for(let i = 0; i < teamMembers.length; i++) {
+//     let teamArray = teamMembers[i];
+//     console.log(teamArray.name, teamArray.position, teamArray.picture);
+// }
+
+
+// STAMPO nel DOM i dati dei TEAM MEMBERS
+// CREO CICLO FOR
+const cardsRow = document.querySelector(".row");
+
+let cards = "";
+for(let i = 0; i < teamMembers.length; i++) {
+    const currentMember = teamMembers[i];
+    cards += `
+    <div class="col"> 
+            <div class="card">
+                <img src="img/${currentMember.picture} " class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${currentMember.name} </h5>
+                  <p class="card-text">${currentMember.position} </p>
+                </div>
+            </div>
+        </div>`;
+}
+console.log(cards);
+cardsRow.innerHTML = cards;
+
